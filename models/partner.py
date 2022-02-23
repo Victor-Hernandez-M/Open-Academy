9de1097d020ce5b67e3fcd6745d863cc9dad0105
Victor Hernandez <victorleonardo29@hotmail.com>
2022-02-22 20:40:17 -0600

    Segundo commit

--- models/partner.py
+++ models/partner.py
@@ -3,11 +3,9 @@ from odoo import fields, models
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
 
-    # Add a new column to the res.partner model, by default partners are not
-    # instructors
-    instructor = fields.Boolean("Instructor", default=False)
+    _inherit = 'res.partner'
+    instructor = fields.Boolean("Instructor")
 
     session_ids = fields.Many2many('openacademy.session',
         string="Attended Sessions", readonly=True)
